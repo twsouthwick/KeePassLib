413437b41f68832042cd8f5402c4dadfedbbf55e
Taylor Southwick <twsouthwick@outlook.com>
2016-09-23 16:36:18 -0700

    Add extensions to aid in portability

---
+++ KeePassLib/Portability/PortableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+using System;
+using System.Security.Cryptography;
+
+namespace KeePassLib
+{
+    internal static class PortableExtensions
+    {
+        public static void Close(this IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
