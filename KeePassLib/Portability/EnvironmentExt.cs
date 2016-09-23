694a41a48c6a2ffa135eb24e9e5e221033bf2252
Taylor Southwick <twsouthwick@outlook.com>
2016-09-23 16:33:02 -0700

    Add EnvironmentExt

---
+++ KeePassLib/Portability/EnvironmentExt.cs
@@ -0,0 +1,14 @@
+﻿
+namespace KeePassLib
+{
+    internal static class EnvironmentExt
+    {
+        public static string UserName { get; } = "userName";
+
+        public static string MachineName { get; } = "machineName";
+
+        public static string UserDomainName { get; } = "domainName";
+
+        public static string AppDataRoamingFolderPath { get; } = "roamingData";
+    }
+}
