2a5bd650988902480f9f9f80d0d49266ad853527
Taylor Southwick <twsouthwick@outlook.com>
2016-09-23 16:28:57 -0700

    Add MessageService

---
+++ KeePassLib/Portability/MessageService.cs
@@ -0,0 +1,10 @@
+﻿namespace KeePassLib
+{
+#if !FEATURE_MESSAGESERVICE
+    internal static class MessageService
+    {
+        public static string NewLine = "\n";
+        public static string NewParagraph = "\r\n";
+    }
+#endif
+}
