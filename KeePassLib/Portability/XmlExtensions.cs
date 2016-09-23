a6aaee683ebf1fdd1e754e6dcb35e5785fe76321
Taylor Southwick <twsouthwick@outlook.com>
2016-09-23 16:01:44 -0700

    Add extension for XmlReader.ReadString()

---
+++ KeePassLib/Portability/XmlExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Xml;
+
+namespace KeePassLib
+{
+    internal static class XmlExtensions
+    {
+        static private uint IsTextualNodeBitmap = 0x6018;
+
+        public static string ReadElementString(this XmlReader reader)
+        {
+            string result = string.Empty;
+
+            if (reader.MoveToContent() != XmlNodeType.Element)
+            {
+                throw new InvalidOperationException();
+            }
+            if (!reader.IsEmptyElement)
+            {
+                reader.Read();
+                result = reader.ReadString();
+                if (reader.NodeType != XmlNodeType.EndElement)
+                {
+                    throw new InvalidOperationException();
+                }
+                reader.Read();
+            }
+            else
+            {
+                reader.Read();
+            }
+            return result;
+        }
+
+        private static string ReadString(this XmlReader reader)
+        {
+            if (reader.ReadState != ReadState.Interactive)
+            {
+                return string.Empty;
+            }
+            reader.MoveToElement();
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                if (reader.IsEmptyElement)
+                {
+                    return string.Empty;
+                }
+                else if (!reader.Read())
+                {
+                    throw new InvalidOperationException();
+                }
+                if (reader.NodeType == XmlNodeType.EndElement)
+                {
+                    return string.Empty;
+                }
+            }
+            string result = string.Empty;
+            while (IsTextualNode(reader.NodeType))
+            {
+                result += reader.Value;
+                if (!reader.Read())
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+
+        static internal bool IsTextualNode(XmlNodeType nodeType)
+        {
+            return 0 != (IsTextualNodeBitmap & (1 << (int)nodeType));
+        }
+    }
+}
