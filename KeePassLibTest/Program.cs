f8680023b3612fcf00594cde2f4077c32a8dbfdc
Taylor Southwick <twsouthwick@outlook.com>
2016-09-26 13:55:44 -0700

    Add tests

---
+++ KeePassLibTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using System.Threading.Tasks;
+using KeePassLib.Serialization;
+using KeePassLib;
+using KeePassLib.Keys;
+using System.IO;
+using Xunit.Abstractions;
+
+namespace KeePassLibTest
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private static string GetFullPath(string path)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "Assets", path);
+        }
+
+        [InlineData("password-aes_rijndael_256.kdbx", "12345", false, 2)]
+        [InlineData("password-key-aes_rijndael_256.kdbx", "12345", true, 2)]
+        [InlineData("key-aes_rijndael_256.kdbx", null, true, 2)]
+        [Theory]
+        public void Decryption(string path, string password, bool hasKey, int count)
+        {
+            var key = new CompositeKey();
+
+            if (password != null)
+            {
+                key.AddUserKey(new KcpPassword("12345"));
+            }
+
+            if (hasKey)
+            {
+                var keyPath = $"{Path.GetFileNameWithoutExtension(path)}.key";
+                key.AddUserKey(new KcpKeyFile(GetFullPath(keyPath)));
+            }
+
+            var db = new PwDatabase
+            {
+                MasterKey = key
+            };
+
+            var file = new KdbxFile(db);
+            file.Load(GetFullPath(path), KdbxFormat.Default, null);
+
+            Assert.Equal(count, (int)db.RootGroup.Entries.UCount);
+        }
+    }
+}
